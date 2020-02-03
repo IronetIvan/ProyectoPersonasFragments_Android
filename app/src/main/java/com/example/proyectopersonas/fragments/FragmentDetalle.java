b13189ff175e4d9002307b3f64f1186b02e4c502
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-02-03 19:20:52 +0100

    act menu personas agenda

--- app/src/main/java/com/example/proyectopersonas/fragments/FragmentDetalle.java
+++ app/src/main/java/com/example/proyectopersonas/fragments/FragmentDetalle.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,30 +18,50 @@ import com.example.proyectopersonas.utils.Persona;
 import java.io.Serializable;
 
 public class FragmentDetalle extends Fragment {
+    private ImageView imagen;
+    private TextView nombre, apellido, telefono;
+    static String TAG_ARG1 = "persona";
+    private Persona persona;
 
     public static FragmentDetalle newInstance(Persona persona) {
 
         Bundle bundle = new Bundle();
 
-        FragmentDetalle fragment = new FragmentDetalle();
-        fragment.setArguments(bundle);
+        FragmentDetalle fragmentDetalle = new FragmentDetalle();
+        fragmentDetalle.setArguments(bundle);
         bundle.putSerializable("persona", (Serializable) persona);
-        return fragment;
+        return fragmentDetalle;
     }
 
     @Override
     public void onAttach(Context context) {
 
         super.onAttach(context);
-        Persona persona = (Persona) this.getArguments().get("persona");
-    }
+        if (this.getArguments() != null) {
+            persona = (Persona) this.getArguments().get(TAG_ARG1);
+        }
 
-    //Arrancar el fragment detalle
+        //Arrancar el fragment detalle
+
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragmentdetalle,container,false);
+        View v = inflater.inflate(R.layout.fragmentdetalle, container, false);
+        imagen = v.findViewById(R.id.imagenItem);
+        nombre = v.findViewById(R.id.nombreItem);
+        apellido = v.findViewById(R.id.apellidoDetalle);
+        telefono = v.findViewById(R.id.menu_llamar);
         return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        imagen.setImageResource(persona.getImagen());
+        nombre.setText(persona.getNombre());
+        apellido.setText(persona.getApellido());
     }
 }
